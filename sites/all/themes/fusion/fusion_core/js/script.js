96d5f928d9b511321997e4db580cb48d624d6cac
HongPong <dan.feidt@gmail.com>
2009-12-18 00:53:09 -0600

    Fusion theme for base theme

---
+++ sites/all/themes/fusion/fusion_core/js/script.js
@@ -0,0 +1,89 @@
+// $Id: script.js,v 1.1.2.3 2009/12/02 00:47:46 sociotech Exp $
+
+Drupal.behaviors.fusionEqualheights = function (context) {
+  $("#header-top-wrapper div.equal-heights div.content").equalHeights();
+  $("#header-group-wrapper div.equal-heights div.content").equalHeights();
+  $("#preface-top-wrapper div.equal-heights div.content").equalHeights();
+  $("#preface-bottom div.equal-heights div.content").equalHeights();
+  $("#sidebar-first div.equal-heights div.content").equalHeights();
+  $("#content-top div.equal-heights div.content").equalHeights();
+  $("#content-region div.equal-heights div.content").equalHeights();
+  $("#content-bottom div.equal-heights div.content").equalHeights();
+  $("#node-top div.equal-heights div.content").equalHeights();
+  $("#node-bottom div.equal-heights div.content").equalHeights();
+  $("#sidebar-last div.equal-heights div.content").equalHeights();
+  $("#postscript-top div.equal-heights div.content").equalHeights();
+  $("#postscript-bottom-wrapper div.equal-heights div.content").equalHeights();
+  $("#footer-wrapper div.equal-heights div.content").equalHeights();
+};
+
+Drupal.behaviors.fusionIE6fixes = function (context) {
+  // IE6 & less-specific functions
+  // Add hover class to primary menu li elements on hover
+  if ($.browser.msie && ($.browser.version < 7)) {
+    $('input.form-submit').hover(function() {
+      $(this).addClass('hover');
+      }, function() {
+        $(this).removeClass('hover');
+    });
+    $('#primary-menu ul.sf-menu li.expanded').hover(function() {
+      $(this).addClass('hover');
+      }, function() {
+        $(this).removeClass('hover');
+    });
+  };
+};
+
+Drupal.behaviors.fusionOverlabel = function (context) {
+  $("div.fusion-horiz-login label").overlabel();
+};
+
+Drupal.behaviors.fusionSuperfish = function (context) {
+  $("#primary-menu ul.sf-menu").superfish({
+    hoverClass:  'sfHover',
+    delay:       250,
+    animation:   {opacity:'show',height:'show'},
+    speed:       'fast',
+    autoArrows:  false,
+    dropShadows: false,
+    disableHI:   true
+  }).supposition();
+};
+
+Drupal.behaviors.fusionSuperfishBlocks = function (context) {
+  $("div.block ul.sf-menu").superfish({
+    hoverClass:  'sfHover',
+    delay:       250,
+    animation:   {opacity:'show',height:'show'},
+    speed:       'fast',
+    autoArrows:  false,
+    dropShadows: false,
+    disableHI:   true
+  }).supposition();
+};
+
+Drupal.behaviors.fusionGridMask = function (context) {
+  var grid_width_pos = parseInt($('body').attr('class').indexOf('grid-width-')) + 11;
+  var grid_width = $('body').attr('class').substring(grid_width_pos, grid_width_pos + 2);
+  var grid = '<div id="grid-mask-overlay" class="full-width"><div class="row">';
+  for (i = 1; i <= grid_width; i++) {
+    grid += '<div class="block grid' + grid_width + '-1"><div class="inner"></div></div>';
+  }
+  grid += '</div></div>';
+  $('body.grid-mask-enabled').prepend(grid);
+  $('#grid-mask-overlay .row').addClass('grid' + grid_width + '-' + grid_width);
+  $('#grid-mask-overlay .block .inner').height($('body').height());
+};
+
+Drupal.behaviors.fusionGridMaskToggle = function (context) {
+  $('body.grid-mask-enabled').prepend('<div id="grid-mask-toggle">grid</div>');
+  $('div#grid-mask-toggle')
+    .toggle( function () {
+      $(this).toggleClass('grid-on');
+      $('body').toggleClass('grid-mask');
+    },
+    function() {
+      $(this).toggleClass('grid-on');
+      $('body').toggleClass('grid-mask');
+    });
+};
