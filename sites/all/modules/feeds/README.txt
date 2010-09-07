cc17ab62edcb05cab0c47dfc6efc543ace63186e
Dan Feidt <dan@Swapbeats.com>
2010-09-06 19:46:21 -0500

    simplepie and feeds for pulling content in

--- sites/all/modules/feeds/README.txt
+++ sites/all/modules/feeds/README.txt
@@ -1,4 +1,4 @@
-$Id: README.txt,v 1.15 2009/12/03 21:27:40 alexb Exp $
+$Id: README.txt,v 1.30 2010/07/05 23:51:36 alexb Exp $
 
 
 "It feeds"
@@ -7,17 +7,15 @@ $Id: README.txt,v 1.15 2009/12/03 21:27:40 alexb Exp $
 FEEDS
 =====
 
-The new incarnation of FeedAPI and Feed Element Mapper. Work in progress.
-
-Contact alex_b for details:
-http://drupal.org/user/53995
+An import and aggregation framework for Drupal.
+http://drupal.org/project/feeds
 
 Features
 ========
 
 - Pluggable import configurations consisting of fetchers (get data) parsers
   (read and transform data) and processors (create content on Drupal).
--- HTTP upload.
+-- HTTP upload (with optional PubSubHubbub support).
 -- File upload.
 -- CSV, RSS, Atom parsing.
 -- Creates nodes or terms.
@@ -35,9 +33,13 @@ Features
 Requirements
 ============
 
-- CTools 1.1
-- Drupal 6.14 or higher.
-- PHP 5.2.x
+- CTools 1.x
+  http://drupal.org/project/ctools
+- Drupal 6.x
+  http://drupal.org/project/drupal
+- PHP safe mode is not supported, depending on your Feeds Importer configuration
+  safe mode may cause no problems though.
+- PHP 5.2.x recommended
 
 Installation
 ============
@@ -45,12 +47,29 @@ Installation
 - Install Feeds, Feeds Admin UI and Feeds defaults.
 - Make sure cron is correctly configured http://drupal.org/cron
 - Navigate to admin/build/feeds.
-- Enable one or more default configuration or create your own: from scratch or
-  by cloning.
+- Enable one or more importers, create your own by adding a new one, modify an
+  existing one by clicking on 'override' or copy and modify an existing one by
+  clicking on 'clone'.
 - Go to import/ to import data.
 - To use SimplePie parser, download SimplePie and place simplepie.inc into
-  feeds/libraries.
+  feeds/libraries. Recommended version: 1.2.
   http://simplepie.org/
+
+PubSubHubbub support
+====================
+
+Feeds supports the PubSubHubbub publish/subscribe protocol. Follow these steps
+to set it up for your site.
+http://code.google.com/p/pubsubhubbub/
+
+- Go to admin/build/feeds and edit (override) the importer configuration you
+  would like to use for PubSubHubbub.
+- Choose the HTTP Fetcher if it is not already selected.
+- On the HTTP Fetcher, click on 'settings' and check "Use PubSubHubbub".
+- Optionally you can use a designated hub such as http://superfeedr.com/ or your
+  own. If a designated hub is specified, every feed on this importer
+  configuration will be subscribed to this hub, no matter what the feed itself
+  specifies.
 
 Libraries support
 =================
@@ -62,6 +81,14 @@ external library used at the moment is SimplePie.
 Libraries found in the libraries search path are preferred over libraries in
 feeds/libraries/.
 
+Transliteration support
+=======================
+
+If you plan to store files with Feeds - for instance when storing podcasts
+or images from syndication feeds - it is recommended to enable the
+Transliteration module to avoid issues with non-ASCII characters in file names.
+http://drupal.org/project/transliteration
+
 API Overview
 ============
 
@@ -71,19 +98,23 @@ http://drupal.org/node/622700
 Testing
 =======
 
-For all tests to pass, run tests with the latest releases of these modules
-available in the Drupal search path:
+See "The developer's guide to Feeds":
+http://drupal.org/node/622700
 
-- Views
-- Data
+Debugging
+=========
 
-This list does not include modules that are required by Feeds anyway.
+Set the Drupal variable 'feeds_debug' to TRUE (i. e. using drush). This will
+create a file /tmp/feeds_[my_site_location].log. Use "tail -f" on the command
+line to get a live view of debug output.
+
+Note: at the moment, only PubSubHubbub related actions are logged.
 
 Performance
 ===========
 
-Untested. There is support for drupal_queue module in the works.
-http://drupal.org/node/599180
+See "The site builder's guide to Feeds":
+http://drupal.org/node/622698
 
 Hidden settings
 ===============
@@ -91,6 +122,10 @@ Hidden settings
 Hidden settings are variables that you can define by adding them to the $conf
 array in your settings.php file.
 
+Name:        feeds_debug
+Default:     FALSE
+Description: Set to TRUE for enabling debug output to
+             /DRUPALTMPDIR/feeds_[sitename].log
 
 Name:        feeds_importer_class
 Default:     'FeedsImporter'
@@ -127,6 +162,11 @@ Description: The table used by FeedsDataProcessor to store feed items. Usually a
              FeedsDataProcessor builds a table name from a prefix (feeds_data_)
              and the importer's id ($importer_id). This default table name can
              be overridden by defining a variable with the same name.
+
+Name:        feeds_node_batch_size
+Default:     50
+             The number of nodes feed node processor creates or deletes in one
+             page load.
 
 Glossary
 ========
--- sites/all/modules/feeds/feeds.api.php
+++ sites/all/modules/feeds/feeds.api.php
@@ -1,9 +1,89 @@
 <?php
-// $Id: feeds.api.php,v 1.3 2009/11/02 21:02:36 alexb Exp $
+// $Id: feeds.api.php,v 1.12 2010/07/08 17:19:16 alexb Exp $
 
 /**
- * @file
- * Doxygen API documentation for hooks invoked by Feeds.
+ * @mainpage
+ *
+ * @verbinclude README.txt
+ */
+
+/**
+ * Feeds offers a CTools based plugin API. Fetchers, parsers and processors are
+ * declared to Feeds as plugins.
+ *
+ * @see feeds_feeds_plugins()
+ * @see FeedsFetcher
+ * @see FeedsParser
+ * @see FeedsProcessor
+ *
+ * @defgroup pluginapi Plugin API
+ * @{
+ */
+
+/**
+ * Example of a CTools plugin hook that needs to be implemented to make
+ * hook_feeds_plugins() discoverable by CTools and Feeds. The hook specifies
+ * that the hook_feeds_plugins() returns Feeds Plugin API version 1 style
+ * plugins.
+ */
+function hook_ctools_plugin_api($owner, $api) {
+  if ($owner == 'feeds' && $api == 'plugins') {
+    return array('version' => 1);
+  }
+}
+
+/**
+ * A hook_feeds_plugins() declares available Fetcher, Parser or Processor
+ * plugins to Feeds. For an example look at feeds_feeds_plugin(). For exposing
+ * this hook hook_ctools_plugin_api() MUST be implemented, too.
+ *
+ * @see feeds_feeds_plugin()
+ */
+function hook_feeds_plugins() {
+  $info = array();
+  $info['MyFetcher'] = array(
+    'name' => 'My Fetcher',
+    'description' => 'Fetches my stuff.',
+    'help' => 'More verbose description here. Will be displayed on fetcher selection menu.',
+    'handler' => array(
+      'parent' => 'FeedsFetcher',
+      'class' => 'MyFetcher',
+      'file' => 'MyFetcher.inc',
+      'path' => drupal_get_path('module', 'my_module'), // Feeds will look for MyFetcher.inc in the my_module directory.
+    ),
+  );
+  $info['MyParser'] = array(
+    'name' => 'ODK parser',
+    'description' => 'Parse my stuff.',
+    'help' => 'More verbose description here. Will be displayed on parser selection menu.',
+    'handler' => array(
+      'parent' => 'FeedsParser', // Being directly or indirectly an extension of FeedsParser makes a plugin a parser plugin.
+      'class' => 'MyParser',
+      'file' => 'MyParser.inc',
+      'path' => drupal_get_path('module', 'my_module'),
+    ),
+  );
+  $info['MyProcessor'] = array(
+    'name' => 'ODK parser',
+    'description' => 'Process my stuff.',
+    'help' => 'More verbose description here. Will be displayed on processor selection menu.',
+    'handler' => array(
+      'parent' => 'FeedsProcessor',
+      'class' => 'MyProcessor',
+      'file' => 'MyProcessor.inc',
+      'path' => drupal_get_path('module', 'my_module'),
+    ),
+  );
+  return $info;
+}
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup import Import hooks
+ * @{
  */
 
 /**
@@ -19,8 +99,38 @@ function hook_feeds_after_import(FeedsImporter $importer, FeedsSource $source) {
 }
 
 /**
+ * @}
+ */
+
+/**
+ * @defgroup mappingapi Mapping API
+ * @{
+ */
+
+/**
+ * Alter mapping targets for users. Use this hook to add additional target
+ * options to the mapping form of User processors.
+ *
+ * For an example implementation, see mappers/profile.inc
+ *
+ * @param: &$targets
+ *  Array containing the targets to be offered to the user. Add to this array
+ *  to expose additional options. Remove from this array to suppress options.
+ */
+function hook_feeds_user_processor_targets_alter(&$targets) {
+  $targets['my_user_field'] = array(
+    'name' => t('My custom user field'),
+    'description' => t('Description of what my custom user field does.'),
+    'callback' => 'my_callback',
+  );
+}
+
+/**
  * Alter mapping targets for nodes. Use this hook to add additional target
  * options to the mapping form of Node processors.
+ *
+ * If the key in $targets[] does not correspond to the actual key on the node
+ * object ($node->key), real_target MUST be specified. See mappers/link.inc
  *
  * For an example implementation, see mappers/content.inc
  *
@@ -31,11 +141,17 @@ function hook_feeds_after_import(FeedsImporter $importer, FeedsSource $source) {
  * @param $content_type
  *   The content type of the target node.
  */
-function hook_feeds_node_processor_targets_alter(&$targts, $content_type) {
+function hook_feeds_node_processor_targets_alter(&$targets, $content_type) {
   $targets['my_node_field'] = array(
     'name' => t('My custom node field'),
     'description' => t('Description of what my custom node field does.'),
     'callback' => 'my_callback',
+  );
+  $targets['my_node_field2'] = array(
+    'name' => t('My Second custom node field'),
+    'description' => t('Description of what my second custom node field does.'),
+    'callback' => 'my_callback2',
+    'real_target' => 'my_node_field_two', // Specify real target field on node.
   );
 }
 
@@ -65,3 +181,20 @@ function hook_feeds_term_processor_targets_alter(&$targets, $vid) {
     );
   }
 }
+
+/**
+ * Alter mapping targets for Data table entries. Use this hook to add additional
+ * target options to the mapping form of Data processor.
+ */
+function hook_feeds_data_processor_targets_alter(&$fields, $data_table) {
+  if ($data_table == mymodule_base_table()) {
+    $fields['mytable:category'] = array(
+      'name' => t('Category'),
+      'description' => t('One or more category terms.'),
+    );
+  }
+}
+
+/**
+ * @}
+ */
